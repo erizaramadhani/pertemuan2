67fe159e38cf948a79f0efde23ffc66524dbf62c
Arcule <farhanarsal60@gmail.com>
2022-03-21 13:07:43 +0700

    menambahkan Pertemuan.php

---
+++ Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Farhan M Arsal
+            Saya tinggal di Jagakarsa
+            Saya suka makan tapi ga gemuk-gemuk";
+    }
+}
